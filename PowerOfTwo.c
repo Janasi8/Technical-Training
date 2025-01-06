6e07336a3c6ab69b18ff81ef1a0cfb7716b0adff
Janasi Jaiswal <janasijaiswal@gmail.com>
2025-01-06 20:38:53 +0530

    Create PowerOfTwo.c

---
+++ PowerOfTwo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int isPowerOfTwo(int n) {
+    return (n > 0) && (n & (n - 1)) == 0;
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPowerOfTwo(num)) {
+        printf("%d is a power of two.\n", num);
+    } else {
+        printf("%d is not a power of two.\n", num);
+    }
+    return 0;
+}
